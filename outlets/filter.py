77dec3a6d93489f423bb148bf363dec87f51fc89
gpancardo <ghpancardo@gmail.com>
2025-08-11 11:31:06 -0600

    Compliance with style guide

--- outlets/filter.py
+++ outlets/filter.py
@@ -3,7 +3,6 @@ import pandas as pd
 df=pd.read_csv("result.csv")
 
 filteredOutlets=df.drop_duplicates(["LINK"])
-#Quiero eliminar los outlets con el mismo LINk sin importar nada
 filteredOutlets=filteredOutlets[filteredOutlets["LINK"].notna()]
 
 filteredOutlets.to_csv("result1.csv", index=False)
